fb5efa99986973b883c3dd8f2e2e0f01a3d6c511
Mark Connelly <com0004@gmail.com>
2022-09-19 18:38:39 +1000

    Overhaul background script

--- background.js
+++ background.js
@@ -1,48 +1,63 @@
+/**
+ * Search for browser bookmark with the specified video ID.
+ * Returns an error message if no bookmarks, or multiple bookmarks, were found.
+ *
+ * Using this function with a callback instead of async-await because the
+ * webextension API does not allow an async listener function.
+ */
+function withVideoBookmark(videoId, callabck) {
+  browser.bookmarks.search(videoId).then(async (searching) => {
+    // Validation
+    if (searching.length > 1) {
+      sendResponse({ message: "multiple_bookmarks_found" });
+      return;
+    }
+    if (searching.length === 0) {
+      sendResponse({ message: "no_bookmark_found" });
+      return;
+    }
+    // Run callback with bookmark
+    const bookmark = searching[0];
+    callabck(bookmark);
+  });
+}
+
+/**
+ * Message listener for content script.
+ */
 browser.runtime.onMessage.addListener((request, _, sendResponse) => {
-  const message = request.message;
+  const method = request.method;
   const videoId = request.videoId;
+  const tag = request.tag;
 
-  if (message === "open_options_page") {
-    // Method: Opening options page
-    browser.runtime.openOptionsPage();
-    sendResponse({ message: "opening_options_page" });
-  } else if (message === "get_folders") {
-    // Method: Getting folders for content buttons
-    browser.storage.local.get("destinationFolderId").then(async (data) => {
-      const destinationFolderId = data.destinationFolderId || "unfiled_____";
-      const children = await browser.bookmarks.getChildren(destinationFolderId);
-      sendResponse({
-        message: "getting_folders",
-        folders: children.filter((b) => b.type === "folder"),
+  switch (method) {
+    case "open_options_page":
+      browser.runtime.openOptionsPage();
+      sendResponse({ message: "opening_options_page" });
+      break;
+    case "get_bookmark":
+      withVideoBookmark(videoId, (bookmark) => {
+        sendResponse({ message: "got_bookmark", bookmark, folder });
       });
-    });
-  } else {
-    // Searching for video
-    browser.bookmarks.search(videoId).then(async (searching) => {
-      // Validation
-      if (searching.length > 1) {
-        sendResponse({ message: "multiple_bookmarks_found" });
-        return;
-      }
-      if (searching.length === 0) {
-        sendResponse({ message: "no_bookmark_found" });
-        return;
-      }
-      if (message === "get_bookmark_info") {
-        // Method: Getting bookmark info
-        const bookmark = searching[0];
-        const folder = (await browser.bookmarks.get(bookmark.parentId))[0];
-        sendResponse({ message: "bookmark_found", bookmark, folder });
-      }
-      if (message === "set_bookmark_folder") {
-        // Method: Setting bookmark folder
-        const bookmark = searching[0];
-        const parentId = request.folderId;
-        await browser.bookmarks.move(bookmark.id, { parentId });
-        sendResponse({ message: "bookmark_moved" });
-      }
-    });
+      break;
+    case "list_tags":
+      const { tags } = browser.storage.local.get("tags");
+      sendResponse({ message: "got_tags", tags });
+      break;
+    case "add_tag":
+      withVideoBookmark(videoId, (bookmark) => {
+        // TODO: change bookmark name
+        sendResponse({ message: "added_tag" });
+      });
+      break;
+    case "remove_tag":
+      withVideoBookmark(videoId, (bookmark) => {
+        // TODO: change bookmark name
+        sendResponse({ message: "removed_tag" });
+      });
+      break;
+    default:
+      throw Error("Invalid method");
   }
-
-  return true;
+  return true; // To allow a response delayed by async
 });
