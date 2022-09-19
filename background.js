a8215bdd22d8c299820b91d445136343a3e78964
Mark Connelly <com0004@gmail.com>
2022-09-19 21:32:00 +1000

    Overhaul + update background + content script

--- background.js
+++ background.js
@@ -1,63 +1,59 @@
 /**
- * Search for browser bookmark with the specified video ID.
- * Returns an error message if no bookmarks, or multiple bookmarks, were found.
- *
- * Using this function with a callback instead of async-await because the
- * webextension API does not allow an async listener function.
- */
-function withVideoBookmark(videoId, callabck) {
-  browser.bookmarks.search(videoId).then(async (searching) => {
-    // Validation
-    if (searching.length > 1) {
-      sendResponse({ message: "multiple_bookmarks_found" });
-      return;
-    }
-    if (searching.length === 0) {
-      sendResponse({ message: "no_bookmark_found" });
-      return;
-    }
-    // Run callback with bookmark
-    const bookmark = searching[0];
-    callabck(bookmark);
-  });
-}
-
-/**
  * Message listener for content script.
  */
 browser.runtime.onMessage.addListener((request, _, sendResponse) => {
   const method = request.method;
-  const videoId = request.videoId;
-  const tag = request.tag;
 
   switch (method) {
     case "open_options_page":
       browser.runtime.openOptionsPage();
       sendResponse({ message: "opening_options_page" });
       break;
+
     case "get_bookmark":
-      withVideoBookmark(videoId, (bookmark) => {
-        sendResponse({ message: "got_bookmark", bookmark, folder });
+      const videoId = request.videoId;
+      browser.bookmarks.search(videoId).then(async (searching) => {
+        if (searching.length > 1) {
+          sendResponse({ message: "multiple_bookmarks_found" });
+          return;
+        }
+        if (searching.length === 0) {
+          sendResponse({ message: "no_bookmark_found" });
+          return;
+        }
+        const bookmark = searching[0];
+        sendResponse({ message: "got_bookmark", bookmark });
       });
       break;
-    case "list_tags":
-      const { tags } = browser.storage.local.get("tags");
-      sendResponse({ message: "got_tags", tags });
-      break;
-    case "add_tag":
-      withVideoBookmark(videoId, (bookmark) => {
-        // TODO: change bookmark name
-        sendResponse({ message: "added_tag" });
+
+    case "get_tags":
+      browser.storage.local.clear();
+      browser.storage.local.get("tags").then((data) => {
+        const defaultTags = [
+          ["❤️Love", "🎸Rock", "🎷Jazz", "⚡️Electronic"],
+          ["Good", "Neutral", "Bad"],
+        ];
+        const tags = data.tags || defaultTags;
+        sendResponse({ message: "got_tags", tags });
       });
       break;
-    case "remove_tag":
-      withVideoBookmark(videoId, (bookmark) => {
-        // TODO: change bookmark name
-        sendResponse({ message: "removed_tag" });
+
+    case "toggle_tag":
+      const tag = request.tag;
+      const bookmarkid = request.bookmarkid;
+      browser.bookmarks.get(bookmarkid).then(async (searching) => {
+        const bookmark = searching[0];
+        const tagExp = new RegExp(`( |^)#${tag}( |$)`);
+        const title = tagExp.test(bookmark.title)
+          ? bookmark.title.replace(tagExp, " ").trim() // remove tag if present
+          : bookmark.title + ` #${tag}`; // add tag if not present
+        await browser.bookmarks.update(bookmarkid, { title });
+        sendResponse({ message: "toggled_tag" });
       });
       break;
+
     default:
-      throw Error("Invalid method");
+      throw Error("Invalid method: " + method);
   }
-  return true; // To allow a response delayed by async
+  return true; // To allow a response from an async coroutine
 });
--- content_script.js
+++ content_script.js
@@ -1,95 +1,62 @@
+// Create UI on page-load
+createTagsPanel();
+
+// Ensure UI updates on page refresh/change
+// Note:  cannot make this too high because bookmark search is an intensive operation and will slow
+//        down the background script if the content script/s calls too frequently.
+setInterval(createTagsPanel, 2000);
+
+/**
+ * Build the HTML and inject onto the page.
+ */
 async function createTagsPanel() {
   const queryParams = new URLSearchParams(window.location.search);
   const title = document.title;
   const videoId = queryParams.get("v");
 
-  // Get bookmark folder
-  const bookmarkInfoResponse = await browser.runtime.sendMessage({
-    message: "get_bookmark_info",
+  // Get bookmark
+  const bookmarkResponse = await browser.runtime.sendMessage({
+    method: "get_bookmark",
     videoId,
   });
-  const { bookmark } = bookmarkInfoResponse;
-  const activeFolderId = bookmark?.parentId;
-  const noBookmarkFound = bookmarkInfoResponse.message === "no_bookmark_found";
-  const multipleBookmarksFound =
-    bookmarkInfoResponse.message === "multiple_bookmarks_found";
 
-  // Get available folders
-  const foldersResponse = await browser.runtime.sendMessage({
-    message: "get_folders",
+  // Get tags
+  const tagsResponse = await browser.runtime.sendMessage({
+    method: "get_tags",
   });
-  const folders = foldersResponse.folders;
 
   // Build HTML
   const template = document.createElement("div");
-  template.id = "my-awesome-tagging-buttons";
+  template.id = "youtube-bookmark-tagging-panel";
+  template.innerHTML = panelHTML(bookmarkResponse, tagsResponse, title);
 
-  const folderButtonsHTML = folders
-    .map(
-      (folder) => `
-      <button class="tagging-button ${
-        activeFolderId === folder.id && "tagging-button-active"
-      }"
-        data-folderid="${folder.id}"
-        data-videoid="${videoId}"
-      >
-        ${folder.title}
-      </button>
-      `
-    )
-    .join("");
-  const noBookmarkFoundHTML = `<button class="tagging-button">⚠ No Bookmark Found</button>`;
-  const multipleBookmarksFoundHTML = `<button class="tagging-button">⚠ Multiple Bookmark Found</button>`;
-
-  template.innerHTML = `
-        <div class="tagging-container" style="display: flex; gap: 4px;">
-            <div style="flex-grow: 1; display: flex; flex-wrap: wrap; gap: 4px;">
-              ${
-                noBookmarkFound
-                  ? noBookmarkFoundHTML
-                  : multipleBookmarksFound
-                  ? multipleBookmarksFoundHTML
-                  : folderButtonsHTML
-              }
-            </div>
-            <div>
-              <button class="tagging-button tagging-video-title">
-                ${title}
-              </button>
-            </div>
-            <div>
-              <button id="tagging-options-button" class="tagging-button tagging-options-button">
-                ⚙
-              </button>
-            </div>
-        </div>
-    `;
-
-  // Add button functionality
+  // Handle click events
   template.addEventListener("click", async (event) => {
-    const folderId = event.target.dataset.folderid;
-    const videoId = event.target.dataset.videoid;
+    const tag = event.target.dataset.tag;
+    const bookmarkid = event.target.dataset.bookmarkid;
     const id = event.target.id;
-    if (folderId && videoId) {
+    // If clicked on tag button
+    if (tag && bookmarkid) {
       const res = await browser.runtime.sendMessage({
-        message: "set_bookmark_folder",
-        videoId,
-        folderId,
+        method: "toggle_tag",
+        tag,
+        bookmarkid,
       });
       console.info(res);
     }
+    // If clicked on options button
     if (id === "tagging-options-button") {
       const res = await browser.runtime.sendMessage({
-        message: "open_options_page",
+        method: "open_options_page",
       });
       console.info(res);
     }
-    // Update the UI on click
+    // Refresh the UI
     createTagsPanel();
   });
 
   // Inject HTML
-  const existing = document.getElementById("my-awesome-tagging-buttons");
+  const existing = document.getElementById("youtube-bookmark-tagging-panel");
   if (existing) {
     // Update tagging panel if should change
     if (!existing.isEqualNode(template)) {
@@ -101,10 +68,67 @@ async function createTagsPanel() {
   }
 }
 
-// Create UI on page-load
-createTagsPanel();
+const panelHTML = (bookmarkResponse, tagsResponse, title) => {
+  return `
+    <div class="tagging-container" style="display: flex; gap: 4px;">
+        <div style="display: flex; flex-grow: 1; flex-direction: column; gap: 0px;">
+          ${panelLeftSectionHTML(bookmarkResponse, tagsResponse)}
+        </div>
+        <div>
+          <button class="tagging-button tagging-video-title">
+            ${title}
+          </button>
+        </div>
+        <div>
+          <button id="tagging-options-button" class="tagging-button tagging-options-button">
+            ⚙
+          </button>
+        </div>
+    </div>
+  `;
+};
 
-// Ensure UI updates on page refresh/change
-// Note:  cannot make this too high because bookmark search is an intensive operation and will slow
-//        down the background script if the content script/s calls too frequently.
-setInterval(createTagsPanel, 2000);
+const panelLeftSectionHTML = (bookmarkResponse, tagsResponse) => {
+  switch (bookmarkResponse.message) {
+    case "got_bookmark":
+      const bookmark = bookmarkResponse.bookmark;
+      switch (tagsResponse.message) {
+        case "got_tags":
+          const tags = tagsResponse.tags;
+          console.log(tags);
+          return tagButtonRowsHTML(bookmark, tags);
+        default:
+          return `<button class="tagging-button">⚠ Error Getting Tags</button>`;
+      }
+    case "multiple_bookmarks_found":
+      return `<button class="tagging-button">⚠ Multiple Bookmarks Found</button>`;
+    case "no_bookmark_found":
+      return `<button class="tagging-button">⚠ No Bookmark Found</button>`;
+    default:
+      return `<button class="tagging-button">⚠ Error Getting Bookmark</button>`;
+  }
+};
+
+const tagButtonRowsHTML = (bookmark, tags) => {
+  return tags
+    .map((tagRow) => {
+      return `
+    <div style="display: flex; flex-wrap: wrap; gap: 4px;">
+      ${tagRow.map((tag) => tagButtonHTML(bookmark, tag)).join("")}
+    </div>
+    `;
+    })
+    .join("");
+};
+
+const tagButtonHTML = (bookmark, tag) => {
+  const hasTag = RegExp(`( |^)#${tag}( |$)`).test(bookmark.title);
+  return `
+    <button class="tagging-button ${hasTag && "tagging-button-active"}"
+      data-tag="${tag}"
+      data-bookmarkid="${bookmark.id}"
+    >
+      ${tag}
+    </button>
+  `;
+};
